037f803ab44eb9432ae7969e93c76fca755d5af0
Arpit Purwar <apurwa19@in.ibm.com>
2020-08-17 16:56:53 +0530

    Event Broadcasting

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,9 +11,23 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
     e.preventDefault();
     const msg = e.target.elements.message.value; 
 
-    socket.emit('sendMessage',msg)
+    socket.emit('sendMessage',msg,(err)=>{
+        if(err){
+            return console.log(err);
+        }
+
+        console.log('Delivered');
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click',()=>{
-    
+    if(!navigator.geolocation){
+        return alert('Geo location is not supported by your browser.!')
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        console.log('Position:',"Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude);
+        const positionObject = {Lat:position.coords.latitude, Long : position.coords.longitude };
+        socket.emit('sendLocation',positionObject);
+    })
 })
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const http = require('http');
 const path = require('path')
 const socketio  = require('socket.io');
+const Filter = require('bad-words');
 
 // Take the instance of express Constructor
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const filter = new Filter();
 
 // Take default port or Enviornmental Port
 const port = process.env.PORT || 3000
@@ -26,14 +28,22 @@ io.on('connection',(socket)=>{
     socket.emit('message','Welcome to Real Time Chat Web Application');
     socket.broadcast.emit('message','New user joined the chat');
 
-    socket.on('sendMessage',(message)=>{
-        //socket.emit('countUpdated',count)
+    socket.on('sendMessage',(message,callback)=>{
+        
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed');
+        }
         io.emit('message',message); // Broadcast to each screen
+        callback()
     })
 
+    socket.on('sendLocation',(position)=>{
+        io.emit('message',`Location : ${position.Lat ,position.Long  }`)
+    })
     socket.on('disconnect',()=>{
         io.emit('message','User has left');
     })
+    
 })
 
 // Listen server on Defined Port
