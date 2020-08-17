fcb1e64e638ce2fd2c58d6fdfc3c20d8e5b93484
Arpit Purwar <apurwa19@in.ibm.com>
2020-08-17 10:40:43 +0530

    Socket Conection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 // Install default packages
 const express = require('express');
+const http = require('http');
 const path = require('path')
-
+const socketio  = require('socket.io');
 
 // Take the instance of express Constructor
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Take default port or Enviornmental Port
 const port = process.env.PORT || 3000
+
 
 // Bring static code form public folder
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -15,7 +19,12 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 //Render public folder to UI
 app.use(express.static(publicDirectoryPath))
 
+// Web socket connection
+io.on('connection',()=>{
+    console.log('New Webscoket connection');
+})
+
 // Listen server on Defined Port
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
