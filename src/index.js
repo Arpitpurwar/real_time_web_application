514aaf1f4d134f21226de01e4aa7e9c8bfbfa371
Arpit Purwar <apurwa19@in.ibm.com>
2020-08-17 10:08:34 +0530

    Comments on Page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+// Install default packages
 const express = require('express');
 const path = require('path')
 
 
-
+// Take the instance of express Constructor
 const app = express();
 
-
+// Take default port or Enviornmental Port
 const port = process.env.PORT || 3000
+
+// Bring static code form public folder
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+//Render public folder to UI
 app.use(express.static(publicDirectoryPath))
 
+// Listen server on Defined Port
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
